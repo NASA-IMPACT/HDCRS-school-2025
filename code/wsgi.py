6551c9488df840171fc742701197dc5d6d401964
xhagrg <grg.iksha@hotmail.com>
2025-06-03 14:32:34 -0500

    Fix wsgi

--- code/wsgi.py
+++ code/wsgi.py
@@ -1,7 +1,7 @@
-import predictor as myapp
+import predictor as app
 
 # This is just a simple wrapper for gunicorn to find your app.
 # If you want to change the algorithm file, simply change "predictor" above to the
 # new file.
 
-app = myapp.app
+app = app.app
