9d8d6349fbf001765c235cf5e0d3ff3ea97da5f4
amarouane-ABDELHAK <am0089@uah.edu>
2025-07-16 13:34:15 -0500

    Add build and push

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,22 +1,15 @@
-%%sh
-docker build . --platform linux/amd64 -t sagemaker-build
+#! /bin/bash
 
-export ECR_URL="<account-number>.dkr.ecr.us-west-2.amazonaws.com"
-
+export ECR_URL="${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com"
+export ECR_IMAGE_NAME=hdcrs:latest
 aws ecr get-login-password --region us-west-2 | \
-  docker login --password-stdin --username AWS $ECR_URL
+docker login --password-stdin --username AWS $ECR_URL
 
-docker tag sagemaker-build $ECR_URL/eo_training:latest
+# docker build . --platform linux/amd64 -t $ECR_URL/eo_training:latest
 
-docker push $ECR_URL/eo_training:latest
+# docker push $ECR_URL/eo_training:latest
 
-docker build -f Dockerfile.inference --platform linux/amd64 -t sagemaker-inference
+docker build -f Dockerfile.inference --platform linux/amd64 -t $ECR_URL/$ECR_IMAGE_NAME
 
-export ECR_URL="<account-number>.dkr.ecr.us-west-2.amazonaws.com"
 
-aws ecr get-login-password --region us-west-2 | \
-  docker login --password-stdin --username AWS $ECR_URL
-
-docker tag sagemaker-inference $ECR_URL/prithvi_inference:latest
-
-docker push $ECR_URL/prithvi_inference:latest
+docker push $ECR_URL/$ECR_IMAGE_NAME
