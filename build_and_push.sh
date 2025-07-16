fb5fa8cf5909acc281095a3c6f7e077b83fe5826
amarouane-ABDELHAK <am0089@uah.edu>
2025-07-16 15:21:20 -0500

    Update service

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,8 +2,7 @@
 
 export ECR_URL="${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com"
 export ECR_IMAGE_NAME=hdcrs:latest
-aws ecr get-login-password --region us-west-2 | \
-docker login --password-stdin --username AWS $ECR_URL
+# aws ecr get-login-password --region us-west-2 | docker login --password-stdin --username AWS $ECR_URL
 
 # docker build . --platform linux/amd64 -t $ECR_URL/eo_training:latest
 
